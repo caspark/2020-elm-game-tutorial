830d8ea14ea2ab1f16fa3ff53b91c1fc48d859fc
Eric Taba <etaba@atlassian.com>
2020-11-05 16:05:55 -0800

    solution

--- src/Main.elm
+++ src/Main.elm
@@ -14,8 +14,9 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-
+import Array
 
 
 -- MAIN
@@ -28,43 +29,68 @@ main =
 
 -- MODEL
 
-
-type alias Model =
-    Int
-
+type alias Model = 
+  {
+    history : List Int,
+    curr : Int
+  }
 
 init : Model
 init =
-    0
-
-
+  { history = [], curr = 0 }
 
 -- UPDATE
 
-
 type Msg
     = Increment
-    | Decrement
+    | Decrement | Square | Reset
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            { model |
+              history = (model.curr + 3) :: model.history,
+              curr = model.curr + 3
+            }
 
         Decrement ->
-            model - 1
+            { model |
+              history = (model.curr - 1) :: model.history,
+              curr = model.curr - 1
+            }
+
+        Square ->
+            { model |
+              history = (model.curr * model.curr) :: model.history,
+              curr = model.curr * model.curr
+            }
+
+        Reset ->
+            { model |
+              history = List.drop 1 model.history,
+              curr =  Maybe.withDefault 0 (List.head (List.drop 1 model.history))
+            }
+
 
 
 
 -- VIEW
 
+rowItem: String -> Html Msg
+rowItem id =
+    div []
+        [ text id ]
 
 view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
+        , div [] [ text (String.fromInt (model.curr)) ]
+        , button [ onClick Increment  ] [ text "+" ]
+        , button [ onClick Square ] [ text "^"]
+        , button [ onClick Reset, disabled (List.isEmpty model.history) ] [ text "<-"]
+        , div [] (List.map rowItem ( List.reverse (List.map String.fromInt model.history )))
         ]
+
